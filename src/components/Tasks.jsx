726e04fde40135a79f730d7bd7cbd0e420c6e63d
juliana lima <akemi.hiruma@gmail.com>
2022-07-08 20:43:26 -0300

    task will complete on click

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import Task from "./Task";
 
-const Tasks = ( {tasks} ) => {
+const Tasks = ( {tasks, handleTaskClick} ) => {
 //  tasks estão dentro do objeto prop
   return (
     <>
-      {tasks.map( (task) => (
-        <Task task={task} />
+      {tasks.map((task) => (
+        <Task task={task} handleTaskClick={handleTaskClick} />
       ))}
     </>
   )
