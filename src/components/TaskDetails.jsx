c13cac12b971cee17cdf80fa9cc16904c4b9c84e
juliana lima <akemi.hiruma@gmail.com>
2022-07-23 22:57:30 -0300

    peplaced render for element and changed app.jsx

--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import Button from "./Button";
 
+
 const TaskDetails = () => {
+  const params = useParams()
+  // return the params we use in URL
+  console.log(params);
+
   return (
     <>
       <div className='back-button-container'>
         <Button>Back</Button>
       </div>
       <div className='task-details-container'>
-        <p></p>
+        <h2>{params.taskTitle}</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Sint nam dolor fugit nostrum dolorum porro?
